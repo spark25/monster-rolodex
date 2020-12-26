eb4cd51544586cb165760be0351b0fa18fa54436
Sumit Kumar <sumit.kumar@zensar.com>
2020-12-27 01:58:30 +0530

    Updates

---
+++ precache-manifest.d6327dd79fdcbd085fdbca5bd0444a88.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea0e938afc0e6f335789127581d639d8",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "af634aae30103a70c319",
+    "url": "/monster-rolodex/static/css/main.256c91f4.chunk.css"
+  },
+  {
+    "revision": "fcc776ce74fb018d3836",
+    "url": "/monster-rolodex/static/js/2.3ec67f95.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.3ec67f95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af634aae30103a70c319",
+    "url": "/monster-rolodex/static/js/main.82690db0.chunk.js"
+  },
+  {
+    "revision": "1db901b49b16624dee18",
+    "url": "/monster-rolodex/static/js/runtime-main.b410029c.js"
+  }
+]);
